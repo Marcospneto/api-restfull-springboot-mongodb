d99ad4aec456e4f98fc72178076c1c23d30653fd
Marcos Pereira da Silva Neto <marcosp.neto@unifacol.edu.br>
2024-01-26 10:53:52 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/jvm/workshopmongo/config/Instatiation.java
+++ src/main/java/com/jvm/workshopmongo/config/Instatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jvm.workshopmongo.domain.Post;
 import com.jvm.workshopmongo.domain.User;
+import com.jvm.workshopmongo.dto.AuthorDTO;
 import com.jvm.workshopmongo.repository.PostRepository;
 import com.jvm.workshopmongo.repository.UserRepository;
 
@@ -32,15 +33,19 @@ public class Instatiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+		/*Salvar primeiro os usuarios para que eles tenham um ID proprio criado pelo banco de dados 
+		  e depois fazer a copia para o AuthorDTO (embaixo)*/
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", 
-				"Vou viajar para São Paulo. Abraços!", maria);
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria)); 
+		/*Criei a  classe AuthorDTO para eu ter atributos especificos do author e tive que refatorar
+		  instanciando o author e recebendo o usuario como argumento */
 		
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", 
-				"Acordei feliz hoje", maria);
+				"Acordei feliz hoje", new AuthorDTO(maria));
 		
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
